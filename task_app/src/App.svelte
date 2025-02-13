516b4525ec68dccfa10488aa215aaaa156d3a758
askpravin <askpravinjadhav@gmail.com>
2025-02-13 12:46:45 +0530

    first commit

---
+++ task_app/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import TasksForms from "./components/tasks-forms.svelte";
+  let message = "Tasks App!";
+</script>
+
+<main>
+  <h1>{message}</h1>
+  <TasksForms />
+</main>
+
+<style>
+  main {
+    margin: 1rem auto;
+    max-width: 800px;
+  }
+</style>
---
+++ task_app/src/components/tasks-forms.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  let newTask = $state("");
+  function formSubmitted(e: SubmitEvent) {
+    e.preventDefault();
+    console.log(newTask);
+    console.log("Form submitted");
+  }
+</script>
+
+<form onsubmit={formSubmitted}>
+  <label for="">
+    <input
+      type="text"
+      placeholder="Task name"
+      name="newTask"
+      bind:value={newTask}
+    />
+    <div class="button-container"><button>Add Task</button></div>
+  </label>
+</form>
+
+<style>
+  .button-container {
+    display: flex;
+    justify-content: end;
+  }
+</style>
