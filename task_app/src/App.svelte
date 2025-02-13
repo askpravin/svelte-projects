77be372820cfdf4d3d0d56de94043d8fca099e05
askpravin <askpravinjadhav@gmail.com>
2025-02-13 15:55:05 +0530

    feat: implement task management by adding tasks to the state and displaying them

--- task_app/src/App.svelte
+++ task_app/src/App.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
   import TasksForms from "./components/tasks-forms.svelte";
   let message = "Tasks App!";
+  type Task = {
+    id: string;
+    title: string;
+    done: boolean;
+  };
+  let tasks = $state<Task[]>([]);
   function addTasks(task: string) {
-    console.log(task);
+    tasks.push({
+      id: crypto.randomUUID(),
+      title: task,
+      done: false,
+    });
+  }
 </script>
 
 <main>
   <h1>{message}</h1>
   <TasksForms {addTasks} />
+  <section>
+    {#each tasks as task}
+      <article>
+        {task.title}
+      </article>
+    {/each}
+  </section>
 </main>
 
 <style>
